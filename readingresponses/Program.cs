56cd950e30ac89195d259ffb93f15d014e183f6d
Sakshi Jain <t-sakj@microsoft.com>
2020-06-28 16:26:05 +0530

    Add project files.

---
+++ readingresponses/Program.cs
@@ -0,0 +1,156 @@
+﻿using cosmosdbquery;
+using Microsoft.Azure.Cosmos;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readingresponses
+{
+    public class Key
+    {
+        public string name = "EmailId";
+        public string datatype = "STRING";
+        public string value { get; set; }
+    }
+
+    public class Attribute
+    {
+        public string key { get; set; }
+        public string datatype = "STRING";
+        public string value { get; set; }
+    }
+
+    public class signal
+    {
+        public string impactedEntity = "Contact";
+        public string signalType = "EmailValidationDataUpdate";
+        public string action = "Update";
+        public Key keys { get; set; }
+        public List<Attribute> attributes { get; set; }
+        public object headers { get; set; }
+        public string correlationId { get; set; }
+        public string originatingSystem = "Informatica";
+        public DateTime originatingSystemDate { get; set; }
+        public string internalProcessor = "CVES";
+        public DateTime internalProcessingDate { get; set; }
+        public DateTime signalAcquisitionDate { get; set; }
+    }
+    class Program
+    {
+        public static readonly string EndpointUri = "https://learn-cos.documents.azure.com:443/";
+
+        public static readonly string PrimaryKey = "PS7eKnpOSJ2aInEGeJbiqk4V8Vab7vZx9OFYUUajjDlhLWpnsgmwuASX0sJ6UAibjy9YYqJDWLGiexqyYJ2uaQ==";
+        public CosmosClient cosmosClient;
+        public Database database;
+        public Container containermain;
+        public Container containerretry;
+        public async Task GetReferencing()
+        {
+            this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey, new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            });
+            database = cosmosClient.GetDatabase("mydb");
+            containermain = database.GetContainer("collection1");
+            containerretry = database.GetContainer("mycollection");
+        }
+        static async Task Main(string[] args)
+        {
+            string strFilePath = @"C:\Users\Lenovo\Documents\emailvalidation_Processed.csv";
+            DateTime lastModified = System.IO.File.GetLastWriteTime(strFilePath).ToUniversalTime();
+            DownloadFileFromInformatica();
+            process p = new process();
+            Program pobject = new Program();
+            await pobject.GetReferencing();
+            p.cosmosClient = pobject.cosmosClient;
+            p.database = pobject.database;
+            p.containermain = pobject.containermain;
+            p.containerretry = pobject.containerretry;
+            List<getmainmail> responses = File.ReadAllLines(@"C:\Users\Lenovo\Documents\emailvalidation_Processed.csv").Skip(1).Select(v => FromCsv(v)).ToList();
+            
+            foreach (var value in responses)
+            {
+                value.Id = value.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Email.Complete;
+                if (value.VerifyEmailResponse.VerifyEmailResult.ServiceStatus.StatusNbr != 220&& value.VerifyEmailResponse.VerifyEmailResult.ServiceStatus.StatusNbr!=270)
+                {
+                    signal SignalObject = new signal();
+                    Key keyobject = new Key();
+                    keyobject.value = value.Id;
+                    SignalObject.keys = keyobject;
+                    List<Attribute> attributelist = new List<Attribute>();
+                    Attribute attributeobjectnew = new Attribute();
+                    Attribute attributeobjectold = new Attribute();
+                    Guid g = Guid.NewGuid();
+                    SignalObject.correlationId = g.ToString();
+             
+                    attributeobjectnew.key = "StatusCode.new";
+                    attributeobjectnew.value = value.VerifyEmailResponse.VerifyEmailResult.ServiceStatus.StatusNbr.ToString();
+                    attributeobjectold.key = "StatusCode.old";
+                    attributeobjectold.value = "220";
+                    attributelist.Add(attributeobjectnew);
+                    attributelist.Add(attributeobjectold);
+                    SignalObject.attributes = attributelist;
+                    SignalObject.originatingSystemDate = lastModified;
+                    SignalObject.internalProcessingDate = DateTime.UtcNow;
+                   // SendSignalviaHttp(SignalObject);
+                    await p.AddingAndDeleting(value);
+                }
+            }
+        }
+
+        public static void SendSignalviaHttp(signal signalObject)
+        {
+         
+        }
+
+        static void DownloadFileFromInformatica()
+        {
+            using (var sftp = new SftpClient("sftp.strikeiron.com", 22, "microsofttest","Ch@!jYmW"))
+            {
+                sftp.Connect();
+
+                using (var file = File.OpenWrite(@"C:\Users\Lenovo\Documents\emailvalidation_Processed.csv"))
+                {
+                    sftp.DownloadFile(@"/From_Strikeiron/emailvalidation_Processed.csv", file);
+                }
+
+                sftp.Disconnect();
+            }
+        }
+          
+       static getmainmail FromCsv(string csvLine)
+        {
+            string[] array = csvLine.Split(',');
+            getmainmail getmainmailObj = new getmainmail();
+            VerifyEmailResponse verifyemailresponse = new VerifyEmailResponse();
+            VerifyEmailResult verifyemailres = new VerifyEmailResult();
+            verifyemailresponse.VerifyEmailResult = verifyemailres;
+            ServiceResult servresult = new ServiceResult();
+            ServiceStatus servstatus = new ServiceStatus();
+            Hygiene hygieneobj = new Hygiene();
+            Email mailobject = new Email();
+            Reason Reasonobj = new Reason();
+            verifyemailres.ServiceStatus = servstatus;
+            verifyemailres.ServiceResult = servresult;
+            servresult.Reason = Reasonobj;
+            servresult.Email = mailobject;
+            servresult.Hygiene = hygieneobj;
+            getmainmailObj.VerifyEmailResponse = verifyemailresponse;
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Email.Complete = array[0];
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceStatus.StatusNbr = long.Parse(array[1]);
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceStatus.StatusDescription = array[2];
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Reason.Code = long.Parse(array[3]);
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Reason.Description = array[4];
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Hygiene.HygieneResult = array[5];
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Hygiene.NetProtected = bool.Parse(array[6]);
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Hygiene.NetProtectedBy = array[7];
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Email.LocalPart = array[11];
+            getmainmailObj.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Email.DomainPart = array[12];
+            return getmainmailObj;
+        }
+        }
+}
---
+++ readingresponses/getretryemail.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Globalization;
+using Newtonsoft.Json.Converters;
+using System.Net.Mail;
+using System.IO;
+using OfficeOpenXml;
+using Microsoft.Azure.Cosmos;
+using System.Net;
+using Microsoft.Azure.Cosmos.Linq;
+using Renci.SshNet;
+
+namespace cosmosdbquery
+{
+   
+public class getretryemail
+    {
+        public string statusCode { get; set; }
+        public string reasonCode { get; set; }
+        [JsonProperty(PropertyName = "id")]
+    
+        public string id { get; set; }
+        public string partitionKey { get; set; }
+        public object hashKey { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+    public class getmainmail
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        public VerifyEmailResponse VerifyEmailResponse { get; set; }
+    }
+    public  class VerifyEmailResponse
+    {
+        public VerifyEmailResult VerifyEmailResult { get; set; }
+    }
+    public  class VerifyEmailResult
+    {
+        public ServiceStatus ServiceStatus { get; set; }
+        public ServiceResult ServiceResult { get; set; }
+    }
+
+    public class ServiceResult
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public Email Email { get; set; }
+        public Reason Reason { get; set; }
+        public Hygiene Hygiene { get; set; }
+        public SendRecommendation SendRecommendation { get; set; }
+        public DestinationCountry DestinationCountry { get; set; }
+        public bool Cached { get; set; }
+        public bool Disposable { get; set; }
+        public bool PotentiallyVulgar { get; set; }
+        public bool RoleBased { get; set; }
+        public string EmailSegment { get; set; }
+    }
+    public class DestinationCountry
+    {
+        public long Code { get; set; }
+        public string Alpha2Code { get; set; }
+        public string Alpha3Code { get; set; }
+        public string Name { get; set; }
+    }
+    public class Email
+    {
+        
+        public string Complete { get; set; }
+       
+        public string LocalPart { get; set; }
+        public string DomainPart { get; set; }
+    }
+    public class Hygiene
+    {
+        public string HygieneResult { get; set; }
+        public bool NetProtected { get; set; }
+        public object NetProtectedBy { get; set; }
+    }
+    public  class Reason
+    {
+        public long Code { get; set; }
+        public string Description { get; set; }
+    }
+    public class SendRecommendation
+    {
+        public string Recommendation { get; set; }
+        public long RecommendedRetries { get; set; }
+        public long RecommendedRetryDelaySeconds { get; set; }
+    }
+
+    public  class ServiceStatus
+    {
+        public long StatusNbr { get; set; }
+        public string StatusDescription { get; set; }
+    }
+
+
+    public class process {
+
+        public static readonly string EndpointUri = "https://learn-cos.documents.azure.com:443/";
+
+        public static readonly string PrimaryKey = "PS7eKnpOSJ2aInEGeJbiqk4V8Vab7vZx9OFYUUajjDlhLWpnsgmwuASX0sJ6UAibjy9YYqJDWLGiexqyYJ2uaQ==";
+        public CosmosClient cosmosClient;
+        public Database database;
+        public Container containermain;
+        public Container containerretry;
+        List<getmainmail> listmain = new List<getmainmail>();
+        List<getmainmail> mainquery = new List<getmainmail>();
+        List<getretryemail> listretry = new List<getretryemail>();
+        List<getretryemail> retrysearchlist = new List<getretryemail>();
+        public async Task GetReference()
+        {
+            this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey, new CosmosClientOptions()
+            {
+              ConnectionMode = ConnectionMode.Gateway
+           });
+            database = cosmosClient.GetDatabase("mydb");
+          containermain = database.GetContainer("collection1");
+            containerretry = database.GetContainer("mycollection");
+        }
+        public async Task QueryItemsMainAsync()
+        {
+            var sqlQueryText = "SELECT * FROM c";
+
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator<getmainmail> queryResultSetIterator = this.containermain.GetItemQueryIterator<getmainmail>(queryDefinition);
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<getmainmail> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (getmainmail family in currentResultSet)
+                {
+                   listmain.Add(family);
+                
+                }
+            }
+            //Console.WriteLine(listmain.Count);
+        }
+        public async Task ItemExistsInMainCollection(string id)
+        {
+            var sqlQueryText = $"SELECT * FROM c WHERE c.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Email.Complete =\"{id}\" ";
+          //  Console.WriteLine(sqlQueryText);
+           // Console.ReadKey();
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator<getmainmail> queryResultSetIterator = this.containermain.GetItemQueryIterator<getmainmail>(queryDefinition);
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<getmainmail> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (getmainmail item in currentResultSet)
+                {
+                    mainquery.Add(item);
+
+                }
+            }
+        
+             }
+        public async Task QueryItemsRetryAsync()
+        {
+            var currentdatetime = DateTime.UtcNow;
+            var updatedtime = currentdatetime.AddHours(-26);
+           
+            var sqlQueryText = $"SELECT * FROM c WHERE c.timestamp<\"{updatedtime.ToString("yyyy-MM-ddTHH\\:mm\\:ss.ffffffZ")}\" " ;
+           // Console.WriteLine(sqlQueryText);
+            //Console.ReadKey();
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator<getretryemail> queryResultSetIterator = this.containerretry.GetItemQueryIterator<getretryemail>(queryDefinition);
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<getretryemail> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (getretryemail item in currentResultSet)
+                {
+                    listretry.Add(item);
+              
+                }
+            }
+           // Console.WriteLine(listretry.Count);
+        }
+        public  async Task AddItemstoMainContainer(getmainmail obj)
+        {
+            try
+            {
+                ItemResponse<getmainmail> Response = await containermain.UpsertItemAsync<getmainmail>(obj);
+                Console.WriteLine("updated item in database with id: {0} \n", Response.Resource.VerifyEmailResponse.VerifyEmailResult.ServiceResult.Email.Complete);
+                            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+                    }
+        public async Task UpdateItemstoRetryContainer(getretryemail obj)
+        {
+            try
+            {
+                ItemResponse<getretryemail> Response = await containerretry.UpsertItemAsync<getretryemail>(obj);
+                Console.WriteLine("updated item in database with id: {0} \n", Response.Resource.id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public async Task searchRetry(string id)
+        {
+            retrysearchlist.Clear();
+            var sqlQueryText = $"SELECT * FROM c WHERE c.id =\"{id}\" ";
+            //  Console.WriteLine(sqlQueryText);
+            // Console.ReadKey();
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator<getretryemail> queryResultSetIterator = this.containerretry.GetItemQueryIterator<getretryemail>(queryDefinition);
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<getretryemail> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (getretryemail item in currentResultSet)
+                {
+                    retrysearchlist.Add(item);
+
+                }
+            }
+
+        }
+        public async Task DeleteItem(string id,PartitionKey p)
+        {
+            try
+            {
+                ItemResponse<getretryemail> Response = await containerretry.DeleteItemAsync<getretryemail>(id, p);
+                Console.WriteLine("Deleted item {0}\n", id);
+               // Console.ReadKey();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("item not found\n");
+            }
+        }
+        public async Task AddingAndDeleting(getmainmail obj)
+        {
+
+            await AddItemstoMainContainer(obj);
+            await searchRetry(obj.Id);
+            if (retrysearchlist.Count != 0)
+            {
+                var item = retrysearchlist[0];
+                await DeleteItem(item.id, new PartitionKey(item.reasonCode));
+            }
+            else { Console.WriteLine("item not found"); }
+            
+        }
+
+        public async Task Readfromdb() {
+            mainquery.Clear();
+           
+            await GetReference();
+            await QueryItemsMainAsync();
+            await QueryItemsRetryAsync();
+           
+            List<getretryemail> retrylist = new List<getretryemail>();
+            int i = 0;
+            foreach (var mail in listretry.ToList())
+            {
+                i++;
+               await ItemExistsInMainCollection(mail.id);
+                if(mainquery.Count!=0)
+                 {  Console.WriteLine("item found");
+                   // Console.ReadKey();
+                    await DeleteItem(mail.id, new PartitionKey(mail.reasonCode));
+                }
+                else
+                {
+                    Console.WriteLine("item not found");
+                  //  Console.ReadKey();
+                    retrylist.Add(mail);
+                    mail.timestamp = DateTime.UtcNow;
+                   
+                    await UpdateItemstoRetryContainer(mail);
+                }
+                if (i >= 10)
+                    break;
+            }
+                   
+            WriteExcelDocument(retrylist);
+            SendToInformatica();
+        }
+
+        void SendToInformatica()
+        {
+            string filepath = @"C:\Users\Lenovo\Documents\emailvalidation.xlsx";
+            //Console.WriteLine("Create client Object");
+            using (SftpClient sftpClient = new SftpClient("sftp.strikeiron.com", 22, "microsofttest", "Ch@!jYmW"))
+            {
+                Console.WriteLine("Connect to server");
+                sftpClient.Connect();
+                sftpClient.ChangeDirectory(@"/To_Strikeiron");
+                Console.WriteLine("Creating FileStream object to stream a file");
+                using (FileStream fs = new FileStream(filepath, FileMode.Open))
+                {
+                    sftpClient.BufferSize = 1024;
+                    sftpClient.UploadFile(fs, Path.GetFileName(filepath));
+                }
+
+                Console.ReadKey();
+                sftpClient.Dispose();
+            }
+        }
+
+        void WriteExcelDocument(List<getretryemail> re)
+        {
+
+            using (ExcelPackage excel = new ExcelPackage())
+            {
+                excel.Workbook.Worksheets.Add("Worksheet1");
+                
+                string headerRange = "A0";
+
+                // Target a worksheet
+                var worksheet = excel.Workbook.Worksheets["Worksheet1"];
+                foreach(getretryemail item in re)
+                {
+                    string number = headerRange.Substring(1);
+                 
+                    headerRange = headerRange.Remove(1);
+                    int i = int.Parse(number) + 1;
+                    headerRange += i.ToString();
+               
+                    worksheet.Cells[headerRange].Value = item.id;
+                }
+                FileInfo excelFile = new FileInfo(@"C:\Users\Lenovo\Documents\emailvalidation.xlsx");
+                excel.SaveAs(excelFile);
+                Console.WriteLine("file written");
+            }
+        }
+    }
+
+    }
+
+   
